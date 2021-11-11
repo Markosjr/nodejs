7282005f814e8aeddabd7fa959a73b0197851e34
Marcos Junior <markosjr1969@gmail.com>
2021-11-11 20:27:27 -0200

    Bem vindo no LocalHost

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const porta = 3000;
 
+app.get('/', function (req, res) {
+    res.send ("Bem-Vindo");
+
+});
+
 app.listen(porta, function () {
     console.log("Aplicação rodando na porta " +porta);
 });
