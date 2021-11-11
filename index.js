b08b90b6695470006a6c624472d77f2c1902c904
Marcos Junior <markosjr1969@gmail.com>
2021-11-10 22:21:01 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let mensagem = "Minha primeira aplicação Node.js";
+let dia = 10;
+
+console.log("Hoje é dia ", dia);
+
+dia = 11;
+
+console.log(mensagem);
+console.log(`Hoje é dia: ${dia}`);
+
+console.log("--------------------")
+
+if (dia == 10) {
+    console.log("Dia de Pagamento");
+} else {
+    console.log("Não é dia de pagamento");
+}
