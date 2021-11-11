4001533831d98d2c0b0786a1b5081008d3984a26
Marcos Junior <markosjr1969@gmail.com>
2021-11-11 21:48:38 -0200

    add GET

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
+const usuario = require("./rotas/usuario");
 const porta = 3000;
+
+app.use("/usuario", usuario);
 
 app.get('/:nome', function (req, res) {
     console.log(req.params)
  
     const nome = req.params.nome
 
-    res.send ("Bem- Vindo " + nome);
+    res.send ("Olá  " + nome);
 
 });
 
 app.listen(porta, function () {
     console.log("Aplicação rodando na porta " +porta);
-});
+}); 
